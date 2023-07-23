dc8fdbccc4cbb5cfbe569c04487d8d871d97ed2a
k27dong <me@kefan.me>
2023-07-23 11:59:23 -0400

    add loop

---
+++ src/commands/loop.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder } = require("discord.js")
+const { assert_channel_play_queue } = require("../helper")
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("loop")
+    .setDescription("开启/关闭单曲循环"),
+  async execute(interaction) {
+      let queue = assert_channel_play_queue(interaction)
+
+      queue.looping = !queue.looping
+      await interaction.reply(`单曲循环: ${queue.looping ? "**ON**" : "**OFF**"}`)
+  },
+}
