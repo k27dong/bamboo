17b06478c4db4e22a82c6866ded0030b4985b040
k27dong <me@kefan.me>
2023-07-23 11:05:45 -0400

    update testcases

--- test/api.js
+++ test/api.js
@@ -1,14 +1,16 @@
-const { search_album } = require("../src/api/search_album")
-const { get_album_songs } = require("../src/api/get_album_songs")
-
 const chai = require("chai")
 const expect = chai.expect
 chai.use(require("chai-sorted"))
 
+const { search_album } = require("../src/api/search_album")
+const { get_album_songs } = require("../src/api/get_album_songs")
+const { get_raw_lyric_by_id } = require("../src/api/get_raw_lyric_by_id")
+const { get_song_url_by_id } = require("../src/api/get_song_url_by_id")
+
 // input parameters used for testing
 const VALID_ALBUM_SEARCH_PARAM = "罗大佑"
 const INVALID_ALBUM_SEARCH_PARAM = "锟斤拷锟斤拷烫烫烫烫烫烫"
-const VALID_ALBUM_ID = 36710 // 宝岛咸酸甜
+const VALID_SONG_ID = 370380 // 风透的日子
 const VALID_ALBUM_ITEM = {
   name: "宝岛咸酸甜",
   id: 36710,
@@ -106,22 +108,23 @@ describe("api", () => {
     it("should return objects with correct types of values", function (done) {
       get_album_songs(VALID_ALBUM_ITEM)
         .then((res) => {
-					expect(res).to.be.an("array")
+          expect(res).to.be.an("array")
 
-					res.forEach((item) => {
-						expect(item).to.be.an("object")
-						expect(item).to.have.all.keys('name', 'id', 'ar', 'al', 'source');
+          res.forEach((item) => {
+            expect(item).to.be.an("object")
+            expect(item).to.have.all.keys("name", "id", "ar", "al", "source")
 
-						expect(item.name).to.be.a('string');
-						expect(item.id).to.be.a('number');
-						expect(item.source).to.be.a('string');
-
-						['ar', 'al'].forEach(key => {
-							expect(item[key]).to.be.an('object').that.has.all.keys('name', 'id');
-							expect(item[key].name).to.be.a('string');
-							expect(item[key].id).to.be.a('number');
-						});
-					});
+            expect(item.name).to.be.a("string")
+            expect(item.id).to.be.a("number")
+            expect(item.source).to.be.a("string")
+            ;["ar", "al"].forEach((key) => {
+              expect(item[key])
+                .to.be.an("object")
+                .that.has.all.keys("name", "id")
+              expect(item[key].name).to.be.a("string")
+              expect(item[key].id).to.be.a("number")
+            })
+          })
 
           done()
         })
@@ -131,5 +134,37 @@ describe("api", () => {
     })
   })
 
-  // todo: get_raw_lyric_by_id, get_song_url_by_id
+  describe("get_raw_lyric_by_id", () => {
+    it("should return the unparsed lyric as a string", function (done) {
+      get_raw_lyric_by_id(VALID_SONG_ID)
+        .then((res) => {
+          expect(res).to.be.a("string")
+
+          done()
+        })
+        .catch((err) => {
+          done(err)
+        })
+    })
+  })
+
+  describe("get_song_url_by_id", () => {
+    it("should return a valid url and a status code", function (done) {
+      get_song_url_by_id(VALID_SONG_ID)
+        .then((res) => {
+          expect(res).to.be.an("array")
+          expect(res).to.have.lengthOf(2)
+
+          const [url, code] = res
+          expect(url).to.be.a("string")
+          expect(code).to.be.a("number")
+          expect(url).to.match(/^http(s)?:\/\/.*/)
+
+          done()
+        })
+        .catch((err) => {
+          done(err)
+        })
+    })
+  })
 })
