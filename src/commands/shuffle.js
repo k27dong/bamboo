b79c43f4b4e43614affd05ad749632a15bbea4fb
Kevin Dong <me@kefan.me>
2023-07-23 15:42:57 +0000

    Automated format fix - 20230723154257

--- src/commands/shuffle.js
+++ src/commands/shuffle.js
@@ -6,18 +6,18 @@ module.exports = {
     .setName("shuffle")
     .setDescription("随机打乱播放列表"),
   async execute(interaction) {
-      let queue = assert_channel_play_queue(interaction)
+    let queue = assert_channel_play_queue(interaction)
 
-      if (queue.track.length > 1) {
-        let to_be_shuffled = queue.track.slice(queue.position + 1, queue.length)
-        let shuffled = shuffle(to_be_shuffled)
+    if (queue.track.length > 1) {
+      let to_be_shuffled = queue.track.slice(queue.position + 1, queue.length)
+      let shuffled = shuffle(to_be_shuffled)
 
-        Array.prototype.splice.apply(
-          queue.track,
-          [queue.position + 1, shuffled.length].concat(shuffled)
-        )
-      }
+      Array.prototype.splice.apply(
+        queue.track,
+        [queue.position + 1, shuffled.length].concat(shuffled),
+      )
+    }
 
-      await interaction.reply("done")
+    await interaction.reply("done")
   },
 }
