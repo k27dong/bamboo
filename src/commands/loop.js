6addb40d95cc318871e0064eeb358d762b4ef51e
Kevin Dong <me@kefan.me>
2023-07-23 16:01:13 +0000

    Automated format fix - 20230723160113

--- src/commands/loop.js
+++ src/commands/loop.js
@@ -6,9 +6,9 @@ module.exports = {
     .setName("loop")
     .setDescription("开启/关闭单曲循环"),
   async execute(interaction) {
-      let queue = assert_channel_play_queue(interaction)
+    let queue = assert_channel_play_queue(interaction)
 
-      queue.looping = !queue.looping
-      await interaction.reply(`单曲循环: ${queue.looping ? "**ON**" : "**OFF**"}`)
+    queue.looping = !queue.looping
+    await interaction.reply(`单曲循环: ${queue.looping ? "**ON**" : "**OFF**"}`)
   },
 }
