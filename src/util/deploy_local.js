e48d80b1b4939e9aad335318bd6e1bd7f87f12f2
k27dong <me@kefan.me>
2023-07-24 16:13:59 -0400

    update typo in commands path

--- src/util/deploy_local.js
+++ src/util/deploy_local.js
@@ -5,7 +5,7 @@ const path = require("node:path")
 
 const commands = []
 // Grab all the command files from the commands directory you created earlier
-const commandsPath = path.join(__dirname, "./commands")
+const commandsPath = path.join(__dirname, "../commands")
 const commandFiles = fs
   .readdirSync(commandsPath)
   .filter((file) => file.endsWith(".js"))
