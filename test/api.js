61ada4f385805e6642f09c33bf456227f5f617c6
k27dong <me@kefan.me>
2023-07-27 16:07:55 -0400

    update test case on apis

--- test/api.js
+++ test/api.js
@@ -8,12 +8,20 @@ const { get_raw_lyric_by_id } = require("../src/api/get_raw_lyric_by_id")
 const { get_song_url_by_id } = require("../src/api/get_song_url_by_id")
 const { get_user_profile } = require("../src/api/get_user_profile")
 const { get_user_playlist } = require("../src/api/get_user_playlist")
+const {
+  get_songs_from_playlist,
+} = require("../src/api/get_songs_from_playlist")
 
 // input parameters used for testing
 const VALID_ALBUM_SEARCH_PARAM = "罗大佑"
 const VALID_ALBUM_SEARCH_PARAM_2 = "冀西南林路行"
-const INVALID_SEARCH_PARAM = "平父燈車說只幸已西"
 const VALID_SONG_ID = 370380 // 风透的日子
+const VALID_USERNAME = "麻辣烤鱼别放大葱"
+const VALID_USERID = 258270965
+const VALID_USERID_NO_PLAYLIST = 123123123
+const VALID_PLAYLIST_ID = 2173922287
+const EMPTY_PLAYLIST_ID = 2299457210
+const INVALID_SEARCH_PARAM = "平父燈車說只幸已西"
 const VALID_ALBUM_ITEM = {
   name: "宝岛咸酸甜",
   id: 36710,
@@ -22,13 +30,6 @@ const VALID_ALBUM_ITEM = {
   date: 823104000000,
   ar: "OK男女合唱团",
 }
-const VALID_USERNAME = "麻辣烤鱼别放大葱"
-const VALID_USERID = 258270965
-const NO_PLAYLIST_USERID = 123123123
-
-/**
- * todo: get_user_profile, get_user_playlist, get_songs_from_playlist
- */
 
 describe("api", () => {
   describe("search_album", () => {
@@ -188,68 +189,113 @@ describe("api", () => {
   describe("get_user_profile", () => {
     it("should return the correct user profile", (done) => {
       get_user_profile(VALID_USERNAME)
-      .then((res) => {
-        expect(res).to.be.an("object")
-        expect(res).to.include.keys("nickname", "playlistCount", "signature", "userId")
-        expect(res.nickname).to.be.a("string")
-        expect(res.playlistCount).to.be.a("number")
-        expect(res.signature).to.be.a("string")
-        expect(res.userId).to.be.a("number")
+        .then((res) => {
+          expect(res).to.be.an("object")
+          expect(res).to.include.keys(
+            "nickname",
+            "playlistCount",
+            "signature",
+            "userId"
+          )
+          expect(res.nickname).to.be.a("string")
+          expect(res.playlistCount).to.be.a("number")
+          expect(res.signature).to.be.a("string")
+          expect(res.userId).to.be.a("number")
 
-        done()
-      })
-      .catch((err) => {
-        done(err)
-      })
+          done()
+        })
+        .catch((err) => {
+          done(err)
+        })
     })
 
     it("should return null if no user found", (done) => {
       get_user_profile(INVALID_SEARCH_PARAM)
-      .then((res) => {
-        expect(res).to.be.null
+        .then((res) => {
+          expect(res).to.be.null
 
-        done()
-      })
-      .catch((err) => {
-        done(err)
-      })
+          done()
+        })
+        .catch((err) => {
+          done(err)
+        })
     })
   })
 
   describe("get_user_playlist", () => {
     it("should return an array of playlist objects", (done) => {
       get_user_playlist(VALID_USERID)
-      .then((res) => {
-        expect(res).to.be.an("array")
+        .then((res) => {
+          expect(res).to.be.an("array")
 
-        res.forEach((item) => {
-          expect(item).to.be.an("object")
+          res.forEach((item) => {
+            expect(item).to.be.an("object")
 
-          expect(item).to.have.all.keys("name", "id", "play_count", "count", "cover_img")
-          expect(item.name).to.be.a("string")
-          expect(item.id).to.be.a("number")
-          expect(item.play_count).to.be.a("number")
-          expect(item.count).to.be.a("number")
-          expect(item.cover_img).to.be.a("string")
+            expect(item).to.have.all.keys(
+              "name",
+              "id",
+              "play_count",
+              "count",
+              "cover_img"
+            )
+            expect(item.name).to.be.a("string")
+            expect(item.id).to.be.a("number")
+            expect(item.play_count).to.be.a("number")
+            expect(item.count).to.be.a("number")
+            expect(item.cover_img).to.be.a("string")
+          })
+
+          done()
         })
-
-        done()
-      })
-      .catch((err) => {
-        done(err)
-      })
+        .catch((err) => {
+          done(err)
+        })
     })
 
     it("should return empty array if no playlist found", (done) => {
-      get_user_playlist(NO_PLAYLIST_USERID)
-      .then((res) => {
-        expect(res).to.be.an("array").that.is.empty
+      get_user_playlist(VALID_USERID_NO_PLAYLIST)
+        .then((res) => {
+          expect(res).to.be.an("array").that.is.empty
 
-        done()
-      })
-      .catch((err) => {
-        done(err)
-      })
+          done()
+        })
+        .catch((err) => {
+          done(err)
+        })
+    })
+  })
+
+  describe("get_songs_from_playlist", () => {
+    it("should return the valid playlist songs", (done) => {
+      get_songs_from_playlist({ id: VALID_PLAYLIST_ID })
+        .then((res) => {
+          expect(res).to.be.an("array")
+
+          res.forEach((item) => {
+            expect(item).to.be.an("object")
+            expect(item).to.have.all.keys("name", "id", "ar", "al", "source")
+            expect(item.name).to.be.a("string")
+            expect(item.id).to.be.a("number")
+            expect(item.source).to.be.a("string")
+          })
+
+          done()
+        })
+        .catch((err) => {
+          done(err)
+        })
+    })
+
+    it("should return empty array if playlist is empty", (done) => {
+      get_songs_from_playlist({ id: EMPTY_PLAYLIST_ID })
+        .then((res) => {
+          expect(res).to.be.an("array").that.is.empty
+
+          done()
+        })
+        .catch((err) => {
+          done(err)
+        })
     })
   })
 })
