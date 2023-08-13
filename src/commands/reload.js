99a42395d57f7cf141f3d57eafd53c2dba078c19
Kevin Dong <me@kefan.me>
2023-08-13 22:48:05 +0000

    Automated format fix - 20230813224805

--- src/commands/reload.js
+++ src/commands/reload.js
@@ -2,28 +2,36 @@ const { SlashCommandBuilder } = require("discord.js")
 
 module.exports = {
   data: new SlashCommandBuilder()
-  .setName("reload")
-  .setDescription("重载")
-  .addStringOption(option =>
-    option.setName('command')
-    .setDescription('The command to reload.')
-    .setRequired(true)),
+    .setName("reload")
+    .setDescription("重载")
+    .addStringOption((option) =>
+      option
+        .setName("command")
+        .setDescription("The command to reload.")
+        .setRequired(true),
+    ),
   async execute(interaction) {
-    const command_name = interaction.options.getString('command', true).toLowerCase();
-    const command = interaction.client.commands.get(command_name);
+    const command_name = interaction.options
+      .getString("command", true)
+      .toLowerCase()
+    const command = interaction.client.commands.get(command_name)
 
     if (!command) {
-      return interaction.reply(`There is no command with name \`${command_name}\`!`);
+      return interaction.reply(
+        `There is no command with name \`${command_name}\`!`,
+      )
     }
 
     // remove command from cache
-    delete require.cache[require.resolve(`./${command.data.name}.js`)];
-    await interaction.client.commands.delete(command.data.name);
+    delete require.cache[require.resolve(`./${command.data.name}.js`)]
+    await interaction.client.commands.delete(command.data.name)
 
     // require command again and add it to the cache
-    const new_command = require(`./${command.data.name}.js`);
-    await interaction.client.commands.set(new_command.data.name, new_command);
+    const new_command = require(`./${command.data.name}.js`)
+    await interaction.client.commands.set(new_command.data.name, new_command)
 
-    await interaction.reply(`Command \`${new_command.data.name}\` was reloaded!`);
+    await interaction.reply(
+      `Command \`${new_command.data.name}\` was reloaded!`,
+    )
   },
 }
