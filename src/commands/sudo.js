dc32a73fdd5c5324380f7f83b3d3f9057b2f61f3
Kevin Dong <me@kefan.me>
2023-08-13 18:29:28 +0000

    Automated format fix - 20230813182928

--- src/commands/sudo.js
+++ src/commands/sudo.js
@@ -3,18 +3,18 @@ const { time_convert, code_block } = require("../helper")
 
 module.exports = {
   data: new SlashCommandBuilder()
-  .setName("sudo")
-  .setDescription("命令行")
-  .addStringOption((option) =>
-    option.setName("run").setDescription("commands").setRequired(true),
-  ),
+    .setName("sudo")
+    .setDescription("命令行")
+    .addStringOption((option) =>
+      option.setName("run").setDescription("commands").setRequired(true),
+    ),
   async execute(interaction) {
     class Server {
       constructor(name, id, region, joined) {
-        this.name = name;
-        this.id = id;
-        this.region = region;
-        this.joined = joined;
+        this.name = name
+        this.id = id
+        this.region = region
+        this.joined = joined
       }
     }
 
@@ -38,13 +38,13 @@ module.exports = {
               guild.name,
               guild.id,
               guild.preferredLocale,
-              guild.joinedTimestamp
-            )
+              guild.joinedTimestamp,
+            ),
           )
           count += guild.memberCount
         })
 
-        table.sort(function(a, b) {
+        table.sort(function (a, b) {
           return a.joined - b.joined
         })
 
@@ -55,24 +55,28 @@ module.exports = {
         console.table(table)
         console.log("TOTAL USERS: " + count)
 
-        await interaction.reply(code_block(`Number of servers: ${table.length}\nTotal users: ${count}`))
+        await interaction.reply(
+          code_block(
+            `Number of servers: ${table.length}\nTotal users: ${count}`,
+          ),
+        )
         break
       case "uptime":
         // display the uptime of the bot since last restart
-        let total_seconds = (interaction.client.uptime / 1000);
-        let days = Math.floor(total_seconds / 86400);
-        total_seconds %= 86400;
-        let hours = Math.floor(total_seconds / 3600);
-        total_seconds %= 3600;
-        let minutes = Math.floor(total_seconds / 60);
-        let seconds = Math.floor(total_seconds % 60);
+        let total_seconds = interaction.client.uptime / 1000
+        let days = Math.floor(total_seconds / 86400)
+        total_seconds %= 86400
+        let hours = Math.floor(total_seconds / 3600)
+        total_seconds %= 3600
+        let minutes = Math.floor(total_seconds / 60)
+        let seconds = Math.floor(total_seconds % 60)
 
-        let uptime = '';
+        let uptime = ""
 
-        uptime += days ? `${days} days, ` : '';
-        uptime += hours ? `${hours} hours, ` : '';
-        uptime += minutes ? `${minutes} minutes, ` : '';
-        uptime += seconds ? `${seconds} seconds` : '';
+        uptime += days ? `${days} days, ` : ""
+        uptime += hours ? `${hours} hours, ` : ""
+        uptime += minutes ? `${minutes} minutes, ` : ""
+        uptime += seconds ? `${seconds} seconds` : ""
 
         await interaction.reply(`uptime: ${uptime}`)
         break
