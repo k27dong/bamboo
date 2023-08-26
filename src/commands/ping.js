9d44a2397804657b77668e7d08836fec8bdcb117
Kevin Dong <me@kefan.me>
2023-08-26 05:55:00 +0000

    Automated format fix - 20230826055500

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -5,6 +5,8 @@ module.exports = {
     .setName("ping")
     .setDescription("replies with Pong!"),
   async execute(interaction) {
-    await interaction.reply(`Pong! The latency is ${interaction.client.ws.ping}ms`)
+    await interaction.reply(
+      `Pong! The latency is ${interaction.client.ws.ping}ms`,
+    )
   },
 }
