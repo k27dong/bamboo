5303de74fee4ca493d80cc6084cbbc1f023eeab9
TimG233 <54853713+TimG233@users.noreply.github.com>
2023-11-07 18:06:32 -0500

    misc: correct typos

--- src/commands/loop.js
+++ src/commands/loop.js
@@ -4,7 +4,7 @@ const { assert_channel_play_queue } = require("../helper")
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("loop")
-    .setDescription("开启/关闭单曲循环"),
+    .setDescription("开启/关闭列表循环"),
   async execute(interaction) {
     let queue = assert_channel_play_queue(interaction)
 
